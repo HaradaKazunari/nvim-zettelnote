461d2b5f9f035575324b735049c11d0f90ed37f3
K.Harada <dbxkazuxdb@gmail.com>
2024-06-19 23:45:20 +0900

    fix file_browser

--- lua/zettelnote/fazzy.lua
+++ lua/zettelnote/fazzy.lua
@@ -1,12 +1,18 @@
 local util = require('zettelnote.util')
 local files = require("zettelnote.__files")
+local telescope = require("telescope")
 
 local M = {}
 
 function M.open_note_filer(base_path)
-  local path = util.empty_ignore(base_path)
   util.create_directory_if_not_exists(base_path)
-  vim.cmd("Telescope file_browser file_ignore_patterns=.DS_Store cwd=" .. path)
+  telescope.extensions.file_browser.file_browser({
+    cwd = base_path,
+    respect_gitignore = false,
+    grouped = true,
+    initial_mode = "normal",
+    layout_config = { height = 40 },
+  })
 end
 
 function M.filtering_by_tags(base_path)
